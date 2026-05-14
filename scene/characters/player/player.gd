06e33a42b5518e5b265cc5e7f65e0cd12b84b6ca
Nyan-pixel <nyanlhtike.01@gmail.com>
2026-05-15 00:25:48 +0630

    Nyan
    Tool Panel

--- scene/characters/player/player.gd
+++ scene/characters/player/player.gd
@@ -4,3 +4,5 @@ extends CharacterBody2D
 @export var current_tool: DataTypes.Tools = DataTypes.Tools.None
 
 var player_direction:Vector2
+
+	
---
+++ scene/ui/tools_panel.gd
@@ -0,0 +1,6 @@
+extends PanelContainer
+
+@onready var tool_axe: Button = $MarginContainer/HBoxContainer/ToolAxe
+@onready var tool_watering: Button = $MarginContainer/HBoxContainer/ToolWatering
+@onready var corn: Button = $MarginContainer/HBoxContainer/Corn
+@onready var tomato: Button = $MarginContainer/HBoxContainer/Tomato
