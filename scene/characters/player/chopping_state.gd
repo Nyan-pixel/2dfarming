baacd3c0b8e96139e2309aa8a07d3e8f10279247
Nyan-pixel <nyanlhtike.01@gmail.com>
2026-05-31 12:00:47 +0630

    Nyan
    Crop growth corn

--- scene/characters/player/chopping_state.gd
+++ scene/characters/player/chopping_state.gd
@@ -7,6 +7,7 @@ extends NodeState
 func _ready() -> void:
 	hit_component_collision_shape.disabled = true
 	hit_component_collision_shape.position = Vector2(0, 0);
+
 func _on_process(_delta : float) -> void:
 	pass
 
@@ -23,19 +24,19 @@ func _on_next_transitions() -> void:
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
 		animated_sprite_2D.play("chopping_back")
-		hit_component_collision_shape.position = Vector2(0, 0)
+		hit_component_collision_shape.position = Vector2(0, -18)
 	elif   player.player_direction == Vector2.DOWN:
 		animated_sprite_2D.play("chopping_front")
-		hit_component_collision_shape.position = Vector2(0, 0)
+		hit_component_collision_shape.position = Vector2(0, 3)
 	elif   player.player_direction == Vector2.RIGHT:
 		animated_sprite_2D.play("chopping_right")
-		hit_component_collision_shape.position = Vector2(13, -3)
+		hit_component_collision_shape.position = Vector2(9, 0)
 	elif   player.player_direction == Vector2.LEFT:
 		animated_sprite_2D.play("chopping_left")
-		hit_component_collision_shape.position = Vector2(-13, -3)
+		hit_component_collision_shape.position = Vector2(-9, 0)
 	else :
 			animated_sprite_2D.play("chopping_front")
-			hit_component_collision_shape.position = Vector2(0, 0)
+			hit_component_collision_shape.position = Vector2(0, 3)
 	
 	hit_component_collision_shape.disabled = false
 
