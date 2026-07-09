8c108f3cccfd1705bd1d6a983fb37208095b905e
Brain-0009 <shwehtunlu093@gmail.com>
2026-07-09 23:26:58 +0630

    noti update
    afd

--- scripts/globals/notification_manager.gd
+++ scripts/globals/notification_manager.gd
@@ -10,3 +10,14 @@ func show(message: String, duration: float = 2.0):
 		notification_ui.show_notification(message, duration)
 	else:
 		push_warning("Notification UI is not registered.")
+
+func show_gold(gold: int):
+	show("+" + str(gold) + " Gold")
+
+
+func show_level(level: int):
+	show("⭐ LEVEL " + str(level) + "!")
+
+
+func show_tool(tool_name: String):
+	show("🔧 " + tool_name + " Unlocked!")
